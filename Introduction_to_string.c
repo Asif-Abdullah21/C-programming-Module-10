847f6b4fc863ff5c8512504ef5419da0e91e8d00
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-16 23:24:59 +0600

    string

--- Introduction_to_string.c
+++ Introduction_to_string.c
@@ -1,18 +1,43 @@
-#include<stdio.h>
-int main()
-{
-    int a[5];
-    char s[5];
+//------------------- String Input ---------------------------
 
-    printf("Showing size in byte: \n");
-    printf("%d\n",sizeof(a));
-    printf("%d\n",sizeof(int));
-    
-    printf("Size of the array: %d\n",sizeof(a)/sizeof(int)); //sizeof(int) = 4;
+// //--------------- string initialize and print ----------------------
 
-    int size = sizeof(s)/sizeof(char);
-    printf("Size of the string: %d\n",size); //sizeof(char) = 1;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     // char s[6] = {'r','a','h','a','t','\0'};
+//     // char s[] = "Rahat\0";
+//     // printf("%d\n",sizeof(s));
+
+//     char s[6] = "Rahat";//size ek besi nite hbe, karon last a ora nijera ekta null bosay dibe
+//     // null character means end of a string
+//     printf("%s\n", s);
+
+//     /*   for (int i = 0; i < 5; i++)
+//         {
+//             printf("%c\n",s[i]);
+//         }    */
+
+//     return 0;
+// }
+
+//--------------------------SIze of an array or a string(array of characters)--------------------------------
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5];
+//     char s[5];
+
+//     printf("Showing size in byte: \n");
+//     printf("%d\n",sizeof(a));
+//     printf("%d\n",sizeof(int));
+
+//     printf("Size of the array: %d\n",sizeof(a)/sizeof(int)); //sizeof(int) = 4;
+
+//     int size = sizeof(s)/sizeof(char);
+//     printf("Size of the string: %d\n",size); //sizeof(char) = 1;
+// }
 
 //------------------------------------------------------------
 
@@ -30,7 +55,6 @@ int main()
 //     {
 //         printf("%c\n",a[i]);
 //     }
-    
-    
+
 //     return 0;
 // }
